273d95d4cd4dc93040026d5725defbc575f56728
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-05-14 16:18:37 +0300

    Feat: Completed the Success Metrics component

--- src/components/SuccessMetrics.tsx
+++ src/components/SuccessMetrics.tsx
@@ -1,6 +1,47 @@
 const SuccessMetrics = () => {
   return (
-    <div>SuccessMetrics</div>
-  )
-}
-export default SuccessMetrics
+    <div className=" w-full h-60 mt-52 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center">
+      <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
+        <div className="flex flex-col">
+          <span className="text-metricsYellow font-bold text-6xl">
+            350 +
+          </span>
+          <span className="text-white font-medium text-xl pt-3">
+            Projects Completed
+          </span>
+        </div>
+      </div>
+      <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
+        <div className="flex flex-col">
+          <span className="text-metricsYellow font-bold text-6xl">
+            350 +
+          </span>
+          <span className="text-white font-medium text-xl pt-3">
+            Projects Completed
+          </span>
+        </div>
+      </div>
+      <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
+        <div className="flex flex-col">
+          <span className="text-metricsYellow font-bold text-6xl">
+            350 +
+          </span>
+          <span className="text-white font-medium text-xl pt-3">
+            Projects Completed
+          </span>
+        </div>
+      </div>
+      <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
+        <div className="flex flex-col">
+          <span className="text-metricsYellow font-bold text-6xl">
+            350 +
+          </span>
+          <span className="text-white font-medium text-xl pt-3">
+            Projects Completed
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default SuccessMetrics;
