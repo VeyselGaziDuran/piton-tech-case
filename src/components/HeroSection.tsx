fef666b1891e32cf90ea2c8635a5bfece007dba3
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-05-14 15:32:30 +0300

    Feat: HeroSection and MeetTheTeam components completed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,25 @@
+import Image from "next/image";
+import React from "react";
+
 const HeroSection = () => {
-  return <div className="bg-yellow-400 h-lvh"></div>;
+  return (
+    <div className="relative bg-heroYellow h-screen flex justify-center items-center overflow-hidden">
+      <div className="inset-0 flex justify-center items-center px-4 py-16 ">
+        <Image
+          src="/images/Shape.png"
+          alt="#"
+          width={1609}
+          height={970}
+          objectFit="cover"
+          className="opacity-70 "
+        />
+      </div>
+      <div className="absolute inset-0 flex justify-center items-center z-10">
+        <span className="font-manrope font-bold text-5xl md:text-7xl lg:text-8xl text-center">
+          Let’s create something <span className="block">great together.</span>
+        </span>
+      </div>
+    </div>
+  );
 };
 export default HeroSection;
--- src/components/MeetTheTeam.tsx
+++ src/components/MeetTheTeam.tsx
@@ -1,6 +1,53 @@
+import Image from "next/image";
+import React from "react";
 const MeetTheTeam = () => {
   return (
-    <div>MeetTheTeam</div>
-  )
-}
-export default MeetTheTeam
+    <div className="pt-16 lg:pt-48 ">
+      <h2 className="font-manrope font-bold text-2xl sm:text-3xl lg:text-4xl flex justify-center py-28">
+        Meet the heros behind the magic
+      </h2>
+      <div className="grid grid-cols-1 xl:grid-cols-3 gap-4 justify-items-center">
+        <div className="flex flex-col items-start justify-center bg-white px-4 pt-4 shadow-md max-w-96">
+          <Image
+            src="/images/Team/image1.png"
+            alt="#"
+            width={390}
+            height={376}
+            objectFit="cover"
+          />
+          <h3 className="font-manrope font-bold text-2xl text-start pt-11">
+            Esther Howard
+          </h3>
+          <p className="font-manrope pt-3 pb-8 opacity-50">Founder</p>
+        </div>
+        <div className="flex flex-col items-start justify-center bg-white px-4 pt-4 shadow-md max-w-96">
+          <Image
+            src="/images/Team/image2.png"
+            alt="#"
+            width={390}
+            height={376}
+            objectFit="cover"
+          />
+          <h3 className="font-manrope font-bold text-2xl text-start pt-11">
+            Cody Fisher
+          </h3>
+          <p className="font-manrope pt-3 pb-8 opacity-50">Developer</p>
+        </div>
+        <div className="flex flex-col items-start justify-center bg-white px-4 pt-4 shadow-md max-w-96">
+          <Image
+            src="/images/Team/image3.png"
+            alt="#"
+            width={390}
+            height={376}
+            objectFit="cover"
+          />
+          <h3 className="font-manrope font-bold text-2xl text-start pt-11">
+            Brooklyn Simmons
+          </h3>
+          <p className="font-manrope pt-3 pb-8 opacity-50">Designer</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MeetTheTeam;
