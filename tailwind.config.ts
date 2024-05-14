038f0f9d775a40da4493b70728266f65331e11c6
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-05-14 14:15:00 +0300

    chore: Add Manrope font and font weights to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,32 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      manrope: ["Manrope", "sans-serif"],
+    },
+    fontWeight: {
+      extralight: "200",
+      light: "300",
+      regular: "400",
+      medium: "500",
+      semibold: "600",
+      bold: "700",
+      extrabold: "800",
+    },
     extend: {
+      colors: {
+        heroYellow: "#FDCA09",
+        metricsYellow: "#FEC00A",
+      },
+      fontSize: {
+        "24": "1.5rem", // 24px
+        "28": "1.75rem", // 28px
+        "30": "1.875rem", // 30px
+        "34": "2.125rem", // 34px
+        "44": "2.75rem", // 44px
+        "60": "3.75rem", // 60px
+        "100": "6.25rem", // 100px
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
