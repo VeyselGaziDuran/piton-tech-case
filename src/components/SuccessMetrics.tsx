1099d93b8729f4c3ac931e4121ef0ea1a6288804
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-05-14 21:54:05 +0300

    Refactor SuccessMetrics component to improve code readability and maintainability

--- src/components/SuccessMetrics.tsx
+++ src/components/SuccessMetrics.tsx
@@ -6,13 +6,21 @@ const SuccessMetrics = () => {
           <div className="flex flex-col">
             <span className="text-metricsYellow font-bold text-6xl">350 +</span>
             <span className="text-white font-medium text-xl pt-3">
-              Projects Completed
+              Clients Worldwide
             </span>
           </div>
         </div>
         <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
           <div className="flex flex-col">
-            <span className="text-metricsYellow font-bold text-6xl">350 +</span>
+            <span className="text-metricsYellow font-bold text-6xl">20 +</span>
+            <span className="text-white font-medium text-xl pt-3">
+              Team Members
+            </span>
+          </div>
+        </div>
+        <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
+          <div className="flex flex-col">
+            <span className="text-metricsYellow font-bold text-6xl">100 +</span>
             <span className="text-white font-medium text-xl pt-3">
               Projects Completed
             </span>
@@ -20,17 +28,9 @@ const SuccessMetrics = () => {
         </div>
         <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
           <div className="flex flex-col">
-            <span className="text-metricsYellow font-bold text-6xl">350 +</span>
+            <span className="text-metricsYellow font-bold text-6xl">85M +</span>
             <span className="text-white font-medium text-xl pt-3">
-              Projects Completed
-            </span>
-          </div>
-        </div>
-        <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
-          <div className="flex flex-col">
-            <span className="text-metricsYellow font-bold text-6xl">350 +</span>
-            <span className="text-white font-medium text-xl pt-3">
-              Projects Completed
+              Revenue Generated
             </span>
           </div>
         </div>
