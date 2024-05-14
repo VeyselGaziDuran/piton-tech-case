1977068e7825b1b0c9d55033b77a110d60934e3b
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-05-14 22:38:38 +0300

    Refactor Into component to improve styling and font sizes

--- src/components/Into.tsx
+++ src/components/Into.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 const Into = () => {
   return (
     <div className="pt-32">
-      <div className="px-28">
-        <h1 className="font-bold text-4xl">Who we are</h1>
-        <p className="font-medium text-3xl pt-9">
+      <div className="px-12 md:px-28 ">
+        <h1 className="font-bold text-4xl text-center md:text-start">Who we are</h1>
+        <p className="font-medium text-xl md:text-3xl pt-9">
           We love what we do and create partnerships with our clients to ensure
           their digital transformation is positioned for long-term success.We
           believe that the human dimensions essential to start any successful
@@ -15,7 +15,7 @@ const Into = () => {
       </div>
 
       <Image
-        className="pt-32 px-24 "
+        className="pt-20 md:pt-32 px-10 md:px-24 mx-auto"
         src="/images/MidImage.png"
         alt="#"
         objectFit="cover"
