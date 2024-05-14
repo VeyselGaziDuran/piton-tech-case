b458dc7fd14fe6afb19e3214f16c2591a0fb3dca
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-05-15 02:17:28 +0300

    Refactor CSS transitions and animations for HeroSection component

--- src/components/MeetTheTeam.tsx
+++ src/components/MeetTheTeam.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import React from "react";
 const MeetTheTeam = () => {
   return (
-    <div className="pt-12 lg:pt-48 ">
+    <div className="pt-12 lg:pt-48 px-3">
       <h2 className="font-bold text-4xl flex text-center justify-center py-14 md:py-28">
         Meet the heros behind the magic
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center">
-        <div className="flex flex-col items-start justify-center bg-white px-2 lg:px-4 pt-2 lg:pt-4 shadow-md max-w-96">
+        <div className="flex flex-col items-start justify-center bg-white px-4 pt-2 lg:pt-4 shadow-md max-w-96">
           <Image
             src="/images/Team/image1.png"
             alt="#"
@@ -19,7 +19,7 @@ const MeetTheTeam = () => {
           <h3 className="font-bold text-2xl text-start pt-11">Esther Howard</h3>
           <p className="pt-3 pb-8 opacity-50">Founder</p>
         </div>
-        <div className="flex flex-col items-start justify-center bg-white px-2 lg:px-4 pt-2 lg:pt-4 shadow-md max-w-96">
+        <div className="flex flex-col items-start justify-center bg-white px-4 pt-2 lg:pt-4 shadow-md max-w-96">
           <Image
             src="/images/Team/image2.png"
             alt="#"
@@ -31,7 +31,7 @@ const MeetTheTeam = () => {
           <h3 className="font-bold text-2xl text-start pt-11">Cody Fisher</h3>
           <p className="pt-3 pb-8 opacity-50">Developer</p>
         </div>
-        <div className="flex flex-col items-start justify-center bg-white px-2 lg:px-4 pt-2 lg:pt-4 shadow-md max-w-96">
+        <div className="flex flex-col items-start justify-center bg-white px-4 pt-2 lg:pt-4 shadow-md max-w-96">
           <Image
             src="/images/Team/image3.png"
             alt="#"
