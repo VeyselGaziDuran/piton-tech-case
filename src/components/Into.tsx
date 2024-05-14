d3b033ff0e211ceca4ec393308255e26dbae79cb
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-05-14 14:15:53 +0300

    feat: Into component completed

--- src/components/Into.tsx
+++ src/components/Into.tsx
@@ -1,6 +1,30 @@
+import Image from "next/image";
+
 const Into = () => {
   return (
-    <div>Into</div>
-  )
-}
-export default Into
+    <div className="pt-[45px] lg:pt-[135px]">
+      <div className="px-28">
+        <h1 className="font-manrope font-semibold lg:font-bold text-44">
+          Who we are
+        </h1>
+        <p className="font-manrope font-regular lg:font-medium text-24 lg:text-30 pt-[12px] lg:pt-[36px] ">
+          We love what we do and create partnerships with our clients to ensure
+          their digital transformation is positioned for long-term success.We
+          believe that the human dimensions essential to start any successful
+          project and that this is where splendid emotional relationships
+          between the company and people are born.
+        </p>
+      </div>
+
+      <Image
+        className="pt-[45px] lg:pt-[135px] px-24 "
+        src="/images/MidImage.png"
+        alt="#"
+        objectFit="cover"
+        width={1528}
+        height={629}
+      />
+    </div>
+  );
+};
+export default Into;
