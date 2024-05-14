d18f7e59d33eaf3c1e60bb4dfabc9079fc19122a
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-05-14 22:23:53 +0300

    Refactor MeetTheTeam component to improve grid layout and image styling

--- src/components/MeetTheTeam.tsx
+++ src/components/MeetTheTeam.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const MeetTheTeam = () => {
   return (
     <div className="pt-16 lg:pt-48 ">
-      <h2 className="font-bold text-2xl sm:text-3xl lg:text-4xl flex justify-center py-28">
+      <h2 className="font-bold text-4xl flex text-center justify-center py-28">
         Meet the heros behind the magic
       </h2>
-      <div className="grid grid-cols-1 xl:grid-cols-3 gap-4 justify-items-center">
-        <div className="flex flex-col items-start justify-center bg-white px-4 pt-4 shadow-md max-w-96">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center">
+        <div className="flex flex-col items-start justify-center bg-white px-2 lg:px-4 pt-2 lg:pt-4 shadow-md max-w-96">
           <Image
             src="/images/Team/image1.png"
             alt="#"
@@ -15,12 +15,10 @@ const MeetTheTeam = () => {
             height={376}
             objectFit="cover"
           />
-          <h3 className="font-bold text-2xl text-start pt-11">
-            Esther Howard
-          </h3>
+          <h3 className="font-bold text-2xl text-start pt-11">Esther Howard</h3>
           <p className="pt-3 pb-8 opacity-50">Founder</p>
         </div>
-        <div className="flex flex-col items-start justify-center bg-white px-4 pt-4 shadow-md max-w-96">
+        <div className="flex flex-col items-start justify-center bg-white px-2 lg:px-4 pt-2 lg:pt-4 shadow-md max-w-96">
           <Image
             src="/images/Team/image2.png"
             alt="#"
@@ -28,12 +26,10 @@ const MeetTheTeam = () => {
             height={376}
             objectFit="cover"
           />
-          <h3 className="font-bold text-2xl text-start pt-11">
-            Cody Fisher
-          </h3>
+          <h3 className="font-bold text-2xl text-start pt-11">Cody Fisher</h3>
           <p className="pt-3 pb-8 opacity-50">Developer</p>
         </div>
-        <div className="flex flex-col items-start justify-center bg-white px-4 pt-4 shadow-md max-w-96">
+        <div className="flex flex-col items-start justify-center bg-white px-2 lg:px-4 pt-2 lg:pt-4 shadow-md max-w-96">
           <Image
             src="/images/Team/image3.png"
             alt="#"
