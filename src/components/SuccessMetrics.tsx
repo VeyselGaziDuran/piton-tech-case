9906265ac2098686b5c8a02e3c67362da113df9e
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-05-14 23:53:35 +0300

    Refactor SuccessMetrics component to improve code readability and maintainability

--- src/components/SuccessMetrics.tsx
+++ src/components/SuccessMetrics.tsx
@@ -4,7 +4,9 @@ const SuccessMetrics = () => {
       <div className="w-full h-60 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center">
         <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
           <div className="flex flex-col">
-            <span className="text-metricsYellow font-bold text-6xl">350 +</span>
+            <span className="text-metricsYellow font-bold text-6xl animate-pulse hover:animate-ping cursor-pointer">
+              350 +
+            </span>
             <span className="text-white font-medium text-xl pt-3">
               Clients Worldwide
             </span>
@@ -12,7 +14,9 @@ const SuccessMetrics = () => {
         </div>
         <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
           <div className="flex flex-col">
-            <span className="text-metricsYellow font-bold text-6xl">20 +</span>
+            <span className="text-metricsYellow font-bold text-6xl animate-pulse hover:animate-ping cursor-pointer">
+              20 +
+            </span>
             <span className="text-white font-medium text-xl pt-3">
               Team Members
             </span>
@@ -20,7 +24,9 @@ const SuccessMetrics = () => {
         </div>
         <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
           <div className="flex flex-col">
-            <span className="text-metricsYellow font-bold text-6xl">100 +</span>
+            <span className="text-metricsYellow font-bold text-6xl animate-pulse hover:animate-ping cursor-pointer">
+              100 +
+            </span>
             <span className="text-white font-medium text-xl pt-3">
               Projects Completed
             </span>
@@ -28,7 +34,9 @@ const SuccessMetrics = () => {
         </div>
         <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
           <div className="flex flex-col">
-            <span className="text-metricsYellow font-bold text-6xl">85M +</span>
+            <span className="text-metricsYellow font-bold text-6xl animate-pulse hover:animate-ping cursor-pointer ">
+              85M +
+            </span>
             <span className="text-white font-medium text-xl pt-3">
               Revenue Generated
             </span>
