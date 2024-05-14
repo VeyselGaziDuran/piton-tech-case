7d2ec2e7d76563c5e4194ac3d51ffd187194fd27
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-05-14 19:27:22 +0300

    Refactor SuccessMetrics component to improve code readability and maintainability

--- src/components/SuccessMetrics.tsx
+++ src/components/SuccessMetrics.tsx
@@ -1,44 +1,38 @@
 const SuccessMetrics = () => {
   return (
-    <div className=" w-full h-60 mt-52 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center">
-      <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
-        <div className="flex flex-col">
-          <span className="text-metricsYellow font-bold text-6xl">
-            350 +
-          </span>
-          <span className="text-white font-medium text-xl pt-3">
-            Projects Completed
-          </span>
+    <div className=" mt-52">
+      <div className="w-full h-60 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center">
+        <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
+          <div className="flex flex-col">
+            <span className="text-metricsYellow font-bold text-6xl">350 +</span>
+            <span className="text-white font-medium text-xl pt-3">
+              Projects Completed
+            </span>
+          </div>
         </div>
-      </div>
-      <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
-        <div className="flex flex-col">
-          <span className="text-metricsYellow font-bold text-6xl">
-            350 +
-          </span>
-          <span className="text-white font-medium text-xl pt-3">
-            Projects Completed
-          </span>
+        <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
+          <div className="flex flex-col">
+            <span className="text-metricsYellow font-bold text-6xl">350 +</span>
+            <span className="text-white font-medium text-xl pt-3">
+              Projects Completed
+            </span>
+          </div>
         </div>
-      </div>
-      <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
-        <div className="flex flex-col">
-          <span className="text-metricsYellow font-bold text-6xl">
-            350 +
-          </span>
-          <span className="text-white font-medium text-xl pt-3">
-            Projects Completed
-          </span>
+        <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
+          <div className="flex flex-col">
+            <span className="text-metricsYellow font-bold text-6xl">350 +</span>
+            <span className="text-white font-medium text-xl pt-3">
+              Projects Completed
+            </span>
+          </div>
         </div>
-      </div>
-      <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
-        <div className="flex flex-col">
-          <span className="text-metricsYellow font-bold text-6xl">
-            350 +
-          </span>
-          <span className="text-white font-medium text-xl pt-3">
-            Projects Completed
-          </span>
+        <div className="bg-black w-full h-60 flex justify-center items-center flex-col">
+          <div className="flex flex-col">
+            <span className="text-metricsYellow font-bold text-6xl">350 +</span>
+            <span className="text-white font-medium text-xl pt-3">
+              Projects Completed
+            </span>
+          </div>
         </div>
       </div>
     </div>
